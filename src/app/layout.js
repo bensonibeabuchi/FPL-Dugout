10cb8ac9b955f6e085582c0fa54fc86a0008a87d
bensonibeabuchi <ibeabuchibenson@gmail.com>
2025-03-09 20:36:46 -0700

    added google adsense

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import ClientProvider from "./components/common/ClientProvider";
+import Head from "next/head";
 
 
 
@@ -15,15 +16,28 @@ const poppins = Poppins({
 export const metadata = {
   title: "FPL Dugout",
   description: "The Ultimate Fantasy Experience",
+  googleAdsenseAccount: "ca-pub-5444473500763929",
+  applicationName: 'FPLDugout',
+  keywords: ['FPL', 'Fantasy Football', 'Football', 'League', 'EPL', 'Premier League', 'Liverpool', 'Arsenal', 'Manchester United', 'Manchester City', 'Chelsea', 'Dugout', 'Fantasy Football League'],
+  authors: [{ name: 'Benson Ibeabuchi' }, { name: 'Benson Ibeabuchi', url: 'https://bensonibeabuchiportfolio.vercel.app/' }],
+  creator: 'Benson Ibeabuchi',
+  publisher: 'Benson Ibeabuchi',
+  siteName: 'FPL Dugout',
+  type: 'website',
+  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Add Google AdSense script */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5444473500763929"
+     crossorigin="anonymous"></script>
+        <meta name="google-adsense-account" content="ca-pub-5444473500763929" />
+      </head>
+      
       <body className={poppins.className}>
-        {/* <Provider store={store}>
-          {children}
-        </Provider> */}
         <ClientProvider>
           {children}
         </ClientProvider>
